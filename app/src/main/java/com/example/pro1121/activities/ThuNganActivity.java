021470c3aabe29abcfec4c589b0878e15ac43d0b
pvd2003 <117169476+pvd2003@users.noreply.github.com>
2022-11-22 15:37:21 +0700

    Phan Van Duc

--- app/src/main/java/com/example/pro1121/activities/ThuNganActivity.java
+++ app/src/main/java/com/example/pro1121/activities/ThuNganActivity.java
@@ -2,6 +2,7 @@ package com.example.pro1121.activities;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
@@ -11,13 +12,19 @@ import androidx.fragment.app.FragmentManager;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.FrameLayout;
+import android.widget.Toast;
 
 import com.example.pro1121.R;
 import com.example.pro1121.fragments.FragmentDoanhThu;
 import com.example.pro1121.fragments.FragmentQLDonHang;
 import com.example.pro1121.fragments.FragmentQLLoaiSanPham;
+import com.example.pro1121.fragments.FragmentQLSanPham;
 import com.example.pro1121.fragments.FragmentQLTaiKhoan;
 import com.example.pro1121.fragments.FragmentTop10;
 import com.google.android.material.navigation.NavigationView;
@@ -50,7 +57,7 @@ public class ThuNganActivity extends AppCompatActivity {
                 Fragment fragment = null;
                 switch (item.getItemId()) {
                     case R.id.mQLSanPham:
-
+                        fragment = new FragmentQLSanPham();
                         break;
 
                     case R.id.mQLLoaiSanPham:
@@ -75,7 +82,7 @@ public class ThuNganActivity extends AppCompatActivity {
 
                     case R.id.mDoiMatKhau:
                         // Dialog đổi mật khẩu
-
+                        showDialogDoiMatKhau();
                         break;
 
                     case R.id.mThoat:
@@ -93,7 +100,7 @@ public class ThuNganActivity extends AppCompatActivity {
                     manager.beginTransaction().replace(R.id.frameLayout, fragment).commit();
                     toolBar.setTitle(item.getTitle());
                 }
-                drawerLayout.closeDrawer(GravityCompat.START);
+                drawerLayout.closeDrawer(GravityCompat.END);
                 return false;
             }
         });
@@ -102,8 +109,34 @@ public class ThuNganActivity extends AppCompatActivity {
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
         if (item.getItemId() == android.R.id.home) {
-            drawerLayout.openDrawer(GravityCompat.START);
+            drawerLayout.openDrawer(GravityCompat.END);
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    //
+    public void showDialogDoiMatKhau(){
+        AlertDialog.Builder builder = new AlertDialog.Builder(this)
+                .setNegativeButton("OK", null)
+                .setPositiveButton("Close",null);
+        LayoutInflater inflater = getLayoutInflater();
+        View view = inflater.inflate(R.layout.dialog_doi_mat_khau,null);
+        EditText etOldPass = view.findViewById(R.id.etOldPass);
+        EditText etNewPass = view.findViewById(R.id.etNewPass);
+        EditText etRePass = view.findViewById(R.id.etRePass);
+
+        builder.setView(view);
+
+        AlertDialog alertDialog = builder.create();
+        alertDialog.setCancelable(false);
+        alertDialog.show();
+
+        alertDialog.getButton(AlertDialog.BUTTON_NEGATIVE).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(ThuNganActivity.this, "Successful", Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
 }
