45f25a15b401ad9c750cf462cad58de3572fefb6
vinhpqps21063 <phamquangvinh13062003@gmail.com>
2022-11-29 22:32:47 +0700

    Dialog

--- app/src/main/java/com/example/pro1121/activities/CTSPEspressoActivity.java
+++ app/src/main/java/com/example/pro1121/activities/CTSPEspressoActivity.java
@@ -5,9 +5,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 
 import com.example.pro1121.R;
 import com.example.pro1121.activities.HomeActivity;
+import com.example.pro1121.fragments.FragmentHome;
 
 public class CTSPEspressoActivity extends AppCompatActivity {
 
@@ -15,11 +17,5 @@ public class CTSPEspressoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ctspespresso);
-    }
-
-    // Chuyển màn hình từ CTSPEspresso sang Home
-    public void onPrevEspressoClick(View view) {
-        startActivity(new Intent(this, HomeActivity.class));
-        finish();
     }
 }
--- app/src/main/java/com/example/pro1121/activities/HomeActivity.java
+++ app/src/main/java/com/example/pro1121/activities/HomeActivity.java
@@ -10,31 +10,10 @@ import android.widget.ImageView;
 import com.example.pro1121.R;
 
 public class HomeActivity extends AppCompatActivity {
-    private ImageView ivMenuCoffee;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.fragment_home);
-    }
-
-    // Chuyển màn hình từ Home sang Menu
-    public void onMenuCoffeeClick(View view) {
-        startActivity(new Intent(this, MenuActivity.class));
-    }
-
-    // Chuyển màn hình từ Home sang CTSPCappuccino
-    public void onCappuccinoClick(View view) {
-        startActivity(new Intent(this, CTSPCappuccinoActivity.class));
-    }
-
-    // Chuyển màn hình từ Home sang CTSPEspresso
-    public void onEspressoClick(View view) {
-        startActivity(new Intent(this, CTSPEspressoActivity.class));
-    }
-
-    // Chuyển màn hình từ Home sang CTSPLatte
-    public void onLatteClick(View view) {
-        startActivity(new Intent(this, CTSPLatteActivity.class));
     }
 }
